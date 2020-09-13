2befe6c39a2c795bf1ceb8b7730cadbfb8514e8f
Matthew Inamdar <matt@goldspecdigital.com>
2020-09-13 13:31:35 +0100

    Added civi page with notifications content

---
+++ src/views/admin/index/Civi.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <vue-headful :title="`${appName} - Admin: CiviCRM`" />
+
+    <gov-heading size="l">CiviCRM</gov-heading>
+    <ck-loader v-if="loading" />
+    <template v-else>
+      <ck-notifications-table :notifications="notifications" />
+      <gov-body>
+        Page {{ currentPage }} of {{ lastPage }}
+        <gov-link v-if="currentPage > 1" @click="onPrevious">Back</gov-link>&nbsp;<!--
+     --><gov-link v-if="currentPage < lastPage" @click="onNext">Next</gov-link>
+      </gov-body>
+    </template>
+  </div>
+</template>
+
+<script>
+import http from "@/http";
+import CkNotificationsTable from "@/components/CkNotificationsTable";
+
+export default {
+  name: "ListCiviCRM",
+  components: { CkNotificationsTable },
+  data() {
+    return {
+      loading: false,
+      notifications: [],
+      currentPage: 1,
+      lastPage: 1
+    };
+  },
+  methods: {
+    async fetchNotifications() {
+      this.loading = true;
+
+      const { data } = await http.get("/notifications", {
+        params: { page: this.currentPage }
+      });
+      this.notifications = data.data;
+      this.currentPage = data.meta.current_page;
+      this.lastPage = data.meta.last_page;
+
+      this.loading = false;
+    },
+    onNext() {
+      this.currentPage++;
+      this.fetchNotifications();
+    },
+    onPrevious() {
+      this.currentPage--;
+      this.fetchNotifications();
+    }
+  },
+  created() {
+    this.fetchNotifications();
+  }
+};
+</script>
